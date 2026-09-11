ca61bb463769df21fe7a2e1bb979f493ace32909
Patrick7244 <patriciosilerio262@outlook.com>
2026-09-11 03:59:20 -0400

    tasks and behind

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -62,16 +62,39 @@ public_users.get('/', async function (req, res) {
 //});
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-    let isbn = req.params.isbn;
-    if(isbn){
-        return res.send(books[isbn]);
-    }else{
-        return res.status(404).json({message: "Not isbn inserted"})
-    }
+//public_users.get('/isbn/:isbn',function (req, res) {
+//    let isbn = req.params.isbn;
+//    if(isbn){
+//        return res.send(books[isbn]);
+//    }else{
+//        return res.status(404).json({message: "Not isbn inserted"})
+//    }
     //console.log("isbn")
     //let booksfound = books.filter((book)=> book.isbn === isbn);
- });
+ //});
+
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+
+    const getBookByISBN = () => {
+        return new Promise((resolve, reject) => {
+            const book = books[isbn];
+            if (book) {
+                resolve(book);
+            } else {
+                reject("Book not found for ISBN " + isbn);
+            }
+        });
+    };
+
+    getBookByISBN()
+        .then((book) => {
+            res.send(JSON.stringify(book, null, 4));
+        })
+        .catch((error) => {
+            res.status(404).json({ message: error });
+        });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
@@ -81,10 +104,33 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+//public_users.get('/title/:title',function (req, res) {
+ //   const title = req.params.title;
+ //   let booksfound = Object.values(books).filter((book)=> book.title === title);
+//    res.send(booksfound);
+//});
+
+public_users.get('/title/:title', function (req, res) {
     const title = req.params.title;
-    let booksfound = Object.values(books).filter((book)=> book.title === title);
-    res.send(booksfound);
+
+    const getBooksByTitle = () => {
+        return new Promise((resolve, reject) => {
+            const booksfound = Object.values(books).filter((book) => book.title === title);
+            if (booksfound.length > 0) {
+                resolve(booksfound);
+            } else {
+                reject("No books found with title " + title);
+            }
+        });
+    };
+
+    getBooksByTitle()
+        .then((booksfound) => {
+            res.send(JSON.stringify(booksfound, null, 4));
+        })
+        .catch((error) => {
+            res.status(404).json({ message: error });
+        });
 });
 
 //  Get book review
